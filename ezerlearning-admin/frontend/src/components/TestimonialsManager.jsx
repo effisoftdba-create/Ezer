a1e4802434ff2102e799e78479a3337a97da21af
Logesh <kvllogesh4010@gmail.com>
2026-08-14 17:10:47 +0530

    Fix missing useEffect import in TestimonialsManager

--- ezerlearning-admin/frontend/src/components/TestimonialsManager.jsx
+++ ezerlearning-admin/frontend/src/components/TestimonialsManager.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSiteData } from '../context/SiteContext';
 import ImagePickerModal from './ImagePickerModal';
 import TestimonialFormModal from './TestimonialFormModal';
